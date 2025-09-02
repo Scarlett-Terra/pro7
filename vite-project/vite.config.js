7b1485aa5dc08a7eafdd4a1402f52e822d2d5629
Acadia <qwe1456321@yahoo.com.tw>
2025-09-02 17:59:41 +0800

    setup

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/hexSchool-Vue/',   // 要佈署上 GitHub 才能改回 /名稱/ 
+  base:'/',   // 要佈署上 GitHub 才能改回 /hexSchool-Vue/ 
   plugins: [
     vue(),
     vueDevTools(),
